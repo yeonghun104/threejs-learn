2258747ec08c8f64c69f39cc4e2c4129df21ab89
김영훈 <yeonghun104@gmail.com>
2024-03-26 14:08:20 +0900

    debug ui 컴포넌트 생성

--- src/app/constants/config.ts
+++ src/app/constants/config.ts
@@ -16,6 +16,7 @@ export const PAGES = [
     title: "07. Fullscreen and resizing",
   },
   { src: "/lessons/08-geometries", title: "08. Geometries" },
+  { src: "/lessons/09-debug-ui", title: "09. Debug UI" },
   { src: "/lessons/arrow-helper", title: "arrowHelper" },
   { src: "/lessons/vector", title: "vector" },
   { src: "/lessons/vector2", title: "vector2" },
---
+++ src/app/lessons/09-debug-ui/page.tsx
@@ -0,0 +1,85 @@
+"use client";
+import { useEffect, useRef } from "react";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+const Page = () => {
+  const el = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (!el.current) return;
+    el.current.innerHTML = "";
+    const canvas = el.current;
+
+    // Scene
+    const scene = new THREE.Scene();
+
+    // Object
+    const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
+    const material = new THREE.MeshBasicMaterial({
+      color: 0xff0000,
+      wireframe: true,
+    });
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    // Sizes
+    const sizes = {
+      width: 800,
+      height: 600,
+    };
+
+    // Camera
+    const aspectRatio = sizes.width / sizes.height;
+    const camera = new THREE.PerspectiveCamera(75, aspectRatio, 1, 1000);
+
+    camera.position.x = 2;
+    camera.position.y = 2;
+    camera.position.z = 2;
+    camera.lookAt(mesh.position);
+    scene.add(camera);
+
+    // Cursor
+    const cursor = {
+      x: 0,
+      y: 0,
+    };
+
+    window.addEventListener("mousemove", (event) => {
+      cursor.x = event.clientX / sizes.width - 0.5;
+      cursor.y = -(event.clientY / sizes.height - 0.5);
+    });
+
+    // Controls
+    const controls = new OrbitControls(camera, canvas);
+    controls.enableDamping = true;
+    // controls.target.y = 2;
+    controls.update();
+
+    // Renderer
+    const renderer = new THREE.WebGLRenderer({
+      canvas,
+    });
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.render(scene, camera);
+
+    const tick = () => {
+      // Update controls
+      controls.update();
+
+      // // Render
+      // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2;
+      // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2;
+      // camera.position.y = cursor.y * 3;
+      // camera.lookAt(mesh.position);
+      renderer.render(scene, camera);
+
+      // Call tick again on the next frame
+      window.requestAnimationFrame(tick);
+    };
+    tick();
+  }, []);
+
+  return <canvas ref={el}></canvas>;
+};
+export default Page;
